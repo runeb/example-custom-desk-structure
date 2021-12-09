b9a65ca2e6fd166b06037a3faa4c4cb8fdcf17ba
Carolina Gonzalez <carolina.nicole.gonzalez@gmail.com>
2021-12-08 19:41:12 -0700

    fix indent

--- src/initialValueTemplates.js
+++ src/initialValueTemplates.js
@@ -3,16 +3,16 @@ import Schema from 'part:@sanity/base/schema'
 
 //_source contains only document types declared by user
 const templates = Schema._source.types.map(schemaType => 
-	T.template({
+  T.template({
     schemaType: schemaType.name,
-		id: `${schemaType.name}-has-region`,
-		title: `${schemaType.title} by region`,
-		description: `${schemaType.title} for a specific region`,
-		parameters: [{name: 'regionId', type: 'string'}],
-		value: params => ({
-			region: {_type: 'reference', _ref: params.regionId}
-		})
-	}),
+    id: `${schemaType.name}-has-region`,
+    title: `${schemaType.title} by region`,
+    description: `${schemaType.title} for a specific region`,
+    parameters: [{name: 'regionId', type: 'string'}],
+    value: params => ({
+      region: {_type: 'reference', _ref: params.regionId}
+    })
+  }),
 )
   
 
